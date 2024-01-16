6f1e0bddc6d9344f177a8b7d0207ca8cf25f3fd2
Kamil Zyla <kamil@appsilon.com>
2024-01-16 17:19:46 +0100

    feat: Make it nice

--- app/main.R
+++ app/main.R
@@ -17,7 +17,7 @@ server <- function(id) {
       div(
         style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
         tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
+          tags$a("Nice!", href = "https://appsilon.github.io/rhino/")
         )
       )
     })
